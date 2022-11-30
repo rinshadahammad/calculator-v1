6cec80955c7cc49ef7f22a9f8eda77f75a4b4ee7
rinshadahammad <krinshad35@gmail.com>
2022-11-30 14:58:32 +0530

    calculator v1

---
+++ calculator.js
@@ -0,0 +1,13 @@
+ function displayvalue(num){
+    Result.value+=num
+
+ }
+ function allclear(){
+    result.value=""
+ }
+ function evaluateout(){
+    result.value=eval(result.value)
+ }
+ function back(){
+    resul.value=result.value.slice(0,-1)
+ }
